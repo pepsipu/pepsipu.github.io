ca638bee6efb1845939321fbf18039b6ad337813
Sammy Hajhamid <sh@pepsi.pw>
2024-04-10 03:12:54 -0700

    fixed weird viewtransitions bug, code cleaning

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,39 +2,17 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import svelte from "@astrojs/svelte";
-// import sitemap from "@astrojs/sitemap";
+
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import remarkSmartypants from "remark-smartypants";
-// import critters from "astro-critters";
-import compress from "astro-compress";
-import robotsTxt from "astro-robots-txt";
-
-import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://pepsipu.com",
-  integrations: [
-    tailwind(),
-    svelte(),
-    mdx(),
-    purgecss(),
-    // critters(),
-    robotsTxt(),
-    compress(),
-    // sitemap(),
-  ],
+  integrations: [tailwind(), svelte(), mdx()],
   markdown: {
-    remarkPlugins: [
-      remarkMath,
-      [
-        remarkSmartypants,
-        {
-          dashes: "oldschool",
-        },
-      ],
-    ],
+    remarkPlugins: [remarkMath, [remarkSmartypants, { dashes: "oldschool" }]],
     rehypePlugins: [rehypeKatex],
     gfm: true,
   },
